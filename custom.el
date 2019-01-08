492d87d6789566c603d043d577f11bde03cabf04
Kevin Lipe <kevin.lipe@gmail.com>
2019-01-08 12:20:57 -0600

    Refactoring, updating theme and defaults

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(olivetti-body-width 72)
+ '(package-selected-packages
+   '(racket zenburn-theme which-key wc-goal-mode use-package spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti markdown-mode magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme)))
+ ;; set olivetti-mode column width
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
