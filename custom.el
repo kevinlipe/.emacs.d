e42cabfec3d876ada1d653753c817f852525455a
Kevin Lipe <kevin.lipe@gmail.com>
2019-06-05 11:23:04 -0500

    Get package list out of custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(olivetti-body-width 72)
- '(package-selected-packages
-   (quote
-    (major-mode-icons racket zenburn-theme which-key wc-goal-mode use-package spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti markdown-mode magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme))))
  ;; set olivetti-mode column width
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
