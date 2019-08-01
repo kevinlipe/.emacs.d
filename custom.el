8588e7a38b35212fd8f7bfb4de56f25fb7f4489b
Kevin Lipe <kevin.lipe@gmail.com>
2019-08-01 13:55:40 -0500

    Added racket-mode to packages

--- custom.el
+++ custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(olivetti-body-width 72)
- ;; set olivetti-mode column width
-)
+ '(package-selected-packages
+   '(racket-mode zenburn-theme which-key wc-goal-mode use-package try spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti mode-icons markdown-mode major-mode-icons magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme)))
 
- (custom-set-faces
+ 
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
