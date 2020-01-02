25ef870edb9e7db1da7f7c0022a83edbc9f26e16
Kevin Lipe <kevin.lipe@gmail.com>
2020-01-02 14:43:46 -0600

    Custom changes

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(olivetti-body-width 72)
  '(package-selected-packages
-   '(exec-path-from-shell racket-mode zenburn-theme which-key wc-goal-mode use-package try spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti mode-icons markdown-mode major-mode-icons magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme)))
+   '(with-editor request org swiper ivy exec-path-from-shell racket-mode zenburn-theme which-key wc-goal-mode use-package try spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti mode-icons markdown-mode major-mode-icons magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme)))
 
  
 (custom-set-faces
