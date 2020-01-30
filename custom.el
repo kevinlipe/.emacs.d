51504d0c4b7ba2303680dd56c7e3d4bc634417cf
Kevin Lipe <kevin.lipe@gmail.com>
2020-01-30 05:45:02 -0600

    Add general, which-key

--- custom.el
+++ custom.el
@@ -3,9 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
  '(olivetti-body-width 72)
  '(package-selected-packages
-   '(with-editor request org swiper ivy exec-path-from-shell racket-mode zenburn-theme which-key wc-goal-mode use-package try spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti mode-icons markdown-mode major-mode-icons magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme)))
+   (quote
+    (with-editor request org swiper ivy exec-path-from-shell racket-mode zenburn-theme which-key wc-goal-mode use-package try spaceline request-deferred pandoc-mode ox-pandoc org-bullets olivetti mode-icons markdown-mode major-mode-icons magit general expand-region csv-mode counsel base16-theme auto-compile anti-zenburn-theme))))
 
  
 (custom-set-faces
